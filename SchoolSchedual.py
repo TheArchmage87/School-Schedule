ae897545fe2a7743fb869f465f372dfc263229c7
TheArchmage87 <144473122+TheArchmage87@users.noreply.github.com>
2023-12-04 17:07:24 -0500

    Add files via upload

---
+++ SchoolSchedual.py
@@ -0,0 +1,15 @@
+x=input("What day is it?: ")
+a, b, c, d, e, f, g, h, i, j = "Web Design", "History", "Break", "Math", "Lunch", "Science", "PE", "French", "English", "CCT"
+if x=="a" :
+  print(a), print(b), print(c), print(d), print(e), print(f), print(g)
+elif x=="b" :
+  print(h), print(i), print(c), print(a), print(e), print(f), print(b)
+elif x=="c" :
+  print(d), print(g), print(c), print(h), print(e), print(j), print(i)
+elif x=="d" :
+  print(g), print(d), print(c), print(b), print(e), print(f), print(a)
+elif x=="e" :
+  print(b), print(a), print(c), print(i), print(e), print(f), print(h)
+elif x=="f" :
+  print(i), print(h), print(c), print(g), print(e), print(j), print(d)
+input("Press [Enter] to exit.") 
